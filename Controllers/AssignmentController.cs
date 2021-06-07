f1cda3df3834386fdbc6872a3beb17d83f4d5338
thaivo <thai2903@gmail.com>
2021-06-07 15:23:31 -0400

    Added Controllers for Assignment with some implemented action methods.

---
+++ Controllers/AssignmentController.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using TaskTrackingApp.Models;
+
+namespace TaskTrackingApp.Controllers
+{
+    public class AssignmentController : Controller
+    {
+
+        //Code factoring
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        static AssignmentController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44382/api/");
+        }
+        // GET: Assignment
+        public ActionResult List()
+        {
+            //objective: communicate with our developer data api to retrieve a list of developers
+            //curl https://localhost:44324/api/developerdata/listdevelopers
+
+            string url = "assignmentdata/listassignments";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("Status code: "+response.StatusCode);
+
+            IEnumerable<AssignmentDto> assignments = response.Content.ReadAsAsync<IEnumerable<AssignmentDto>>().Result;
+            //Debug.WriteLine("Number of developers received: " + developers.Count());
+            return View(assignments);
+        }
+
+        // GET: Assignment/Details/5
+        public ActionResult Details(int id)
+        {
+            //objective: communicate with our developer data api to retrieve a list of developers
+            //curl https://localhost:44324/api/developerdata/findassignment/id
+
+            string url = "assignmentdata/findassignment/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("Status code: " + response.StatusCode);
+
+            AssignmentDto selectedassignment = response.Content.ReadAsAsync<AssignmentDto>().Result;
+            //Debug.WriteLine("developer selected: " + selecteddeveloper.DeveloperFirstName + " "+selecteddeveloper.DeveloperLastName);
+            return View(selectedassignment);
+        }
+
+        public ActionResult Error()
+        {
+
+            return View();
+        }
+
+        // GET: Assignment/Create
+        public ActionResult New()
+        {
+            //information about all developers in the project.
+            //GET api/developersdata/listdevelopers
+
+            string url = "developerdata/listdevelopers";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            IEnumerable<DeveloperDto> DeveloperOptions = response.Content.ReadAsAsync<IEnumerable<DeveloperDto>>().Result;
+
+            return View(DeveloperOptions);
+        }
+
+        // POST: Assignment/Create
+        [HttpPost]
+        public ActionResult Create(Assignment assignment)
+        {
+            string url = "assignmentdata/addassignment";
+
+
+            string jsonpayload = jss.Serialize(assignment);
+            //Debug.WriteLine(jsonpayload);
+
+            HttpContent content = new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Assignment/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Assignment/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Assignment/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Assignment/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/AssignmentDataController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TaskTrackingApp.Models;
+
+namespace TaskTrackingApp.Controllers
+{
+    public class AssignmentDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/AssignmentData
+        [HttpGet]
+        public IHttpActionResult ListAssignments()
+        {
+            List<Assignment> assignments = db.Assignments.ToList();
+            List<AssignmentDto> assignmentDtos = new List<AssignmentDto>();
+            assignments.ForEach(a => assignmentDtos.Add(new AssignmentDto()
+            {
+                AssignmentID = a.AssignmentID,
+                AssignmentDesc = a.AssignmentDesc,
+                Priority = a.Priority,
+                Status = a.Status,
+                DevID = (a.Developer != null)?(int)a.DevID:0,
+                DeveloperFirstName = (a.Developer != null) ? a.Developer.DeveloperFirstName : "",
+                DeveloperLastName = (a.Developer != null)?a.Developer.DeveloperLastName:""
+            }));
+            return Ok(assignmentDtos);
+        }
+
+        [HttpGet]
+        public IHttpActionResult ListAssignmentsForDeveloper(int id)
+        {
+            List<Assignment> assignments = db.Assignments.Where(a => a.DevID == id).ToList();
+            List<AssignmentDto> assignmentDtos = new List<AssignmentDto>();
+
+            assignments.ForEach(a => assignmentDtos.Add(new AssignmentDto()
+            {
+                AssignmentID = a.AssignmentID,
+                AssignmentDesc = a.AssignmentDesc,
+                DevID = (int)a.DevID,
+                DeveloperFirstName = a.Developer.DeveloperFirstName,
+                DeveloperLastName = a.Developer.DeveloperLastName,
+                Priority = a.Priority,
+                Status = a.Status
+            }));
+            return Ok(assignmentDtos);
+        }
+
+        // GET: api/AssignmentData/5
+        [ResponseType(typeof(Assignment))]
+        [HttpGet]
+        public IHttpActionResult FindAssignment(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            AssignmentDto assignmentDto = new AssignmentDto()
+            {
+                AssignmentID = assignment.AssignmentID,
+                AssignmentDesc = assignment.AssignmentDesc,
+                Priority = assignment.Priority,
+                Status = assignment.Status,
+                DevID = (assignment.Developer != null) ? (int)assignment.DevID : 0,
+                DeveloperFirstName = (assignment.Developer != null) ? assignment.Developer.DeveloperFirstName : "",
+                DeveloperLastName = (assignment.Developer != null) ? assignment.Developer.DeveloperLastName : ""
+            };
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(assignmentDto);
+        }
+
+        // PUT: api/AssignmentData/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateAssignment(int id, Assignment assignment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != assignment.AssignmentID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(assignment).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AssignmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/AssignmentData
+        [ResponseType(typeof(Assignment))]
+        public IHttpActionResult AddAssignment(Assignment assignment)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Assignments.Add(assignment);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = assignment.AssignmentID }, assignment);
+        }
+
+        // DELETE: api/AssignmentData/5
+        [ResponseType(typeof(Assignment))]
+        public IHttpActionResult DeleteAssignment(int id)
+        {
+            Assignment assignment = db.Assignments.Find(id);
+            if (assignment == null)
+            {
+                return NotFound();
+            }
+
+            db.Assignments.Remove(assignment);
+            db.SaveChanges();
+
+            return Ok(assignment);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool AssignmentExists(int id)
+        {
+            return db.Assignments.Count(e => e.AssignmentID == id) > 0;
+        }
+    }
+}
