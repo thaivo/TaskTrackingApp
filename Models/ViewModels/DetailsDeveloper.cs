58b33cdb721abfc44769a940cbea9e42f8dad9be
thaivo <thai2903@gmail.com>
2021-06-07 15:33:08 -0400

    Added DetailsDeveloper ViewModel

---
+++ Models/ViewModels/DetailsDeveloper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskTrackingApp.Models.ViewModels
+{
+    public class DetailsDeveloper
+    {
+        public DeveloperDto selectedDeveloper { get; set; }
+        public IEnumerable<AssignmentDto> assignedTasks { get; set; }
+    }
+}
