7997b28d3adcf6d9f505c6032559af841ca2ea40
thaivo <thai2903@gmail.com>
2021-06-07 10:48:17 -0400

    Insert Key attritbute for SkillID property.

--- Models/Skill.cs
+++ Models/Skill.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.ComponentModel.DataAnnotations;
 namespace TaskTrackingApp.Models
 {
     public class Skill
     {
+        [Key]
         public int SkillID { get; set; }
         public string SkillName { get; set; }
 
