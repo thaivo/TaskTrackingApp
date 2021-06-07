62c892268a0980c3cc902a34f236f5dc3b78d789
thaivo <thai2903@gmail.com>
2021-06-07 15:32:37 -0400

    Created Controllers for Developer and Skill classes

---
+++ Controllers/DeveloperController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using TaskTrackingApp.Models;
+using System.Web.Script.Serialization;
+using TaskTrackingApp.Models.ViewModels;
+
+namespace TaskTrackingApp.Controllers
+{
+    public class DeveloperController : Controller
+    {
+        //Code factoring
+        private static readonly HttpClient client;
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        static DeveloperController()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44382/api/");
+        }
+
+        // GET: Developer/List
+        public ActionResult List()
+        {
+            //objective: communicate with our developer data api to retrieve a list of developers
+            //curl https://localhost:44324/api/developerdata/listdevelopers
+            
+            string url = "developerdata/listdevelopers";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("Status code: "+response.StatusCode);
+
+            IEnumerable<DeveloperDto> developers = response.Content.ReadAsAsync<IEnumerable<DeveloperDto>>().Result;
+            //Debug.WriteLine("Number of developers received: " + developers.Count());
+            return View(developers);
+        }
+
+        // GET: Developer/Details/5
+        public ActionResult Details(int id)
+        {
+            DetailsDeveloper ViewModel = new DetailsDeveloper();
+            //objective: communicate with our developer data api to retrieve a list of developers
+            //curl https://localhost:44324/api/developerdata/listdevelopers
+            
+            string url = "developerdata/finddeveloper/" + id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            //Debug.WriteLine("Status code: " + response.StatusCode);
+
+            DeveloperDto selecteddeveloper = response.Content.ReadAsAsync<DeveloperDto>().Result;
+            //Debug.WriteLine("developer selected: " + selecteddeveloper.DeveloperFirstName + " "+selecteddeveloper.DeveloperLastName);
+            ViewModel.selectedDeveloper = selecteddeveloper;
+
+            //TODO: load the list of assignments of this developer
+            url = "assignmentdata/listassignmentsfordeveloper/" + id;
+            response = client.GetAsync(url).Result;
+            IEnumerable<AssignmentDto> relatedAssignments = response.Content.ReadAsAsync<IEnumerable<AssignmentDto>>().Result;
+            ViewModel.assignedTasks = relatedAssignments;
+            return View(ViewModel);
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
+        // GET: Developer/New
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // POST: Developer/Create
+        [HttpPost]
+        public ActionResult Create(Developer developer)
+        {
+            Debug.WriteLine("Developer: " + developer.DeveloperFirstName + " " + developer.DeveloperLastName);
+            //curl -d @developer.json -H "Content-type:application/json" https://localhost:44382/api/developerdata/adddevloper
+            string url = "developerdata/adddeveloper";
+
+            
+            string jsonpayload = jss.Serialize(developer);
+
+            Debug.WriteLine(jsonpayload);
+
+            HttpContent content= new StringContent(jsonpayload);
+            content.Headers.ContentType.MediaType = "application/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+            
+            
+        }
+
+        // GET: Developer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            string url = "developerdata/finddeveloper/"+id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            Developer selectedDeveloper = response.Content.ReadAsAsync<Developer>().Result;
+            return View(selectedDeveloper);
+        }
+
+        // POST: Developer/Edit/5
+        [HttpPost]
+        public ActionResult Update(int id, Developer developer)
+        {
+            string url = "developerdata/updatedeveloper/" + id;
+            string jsonPayload = jss.Serialize(developer);
+            HttpContent content = new StringContent(jsonPayload);
+            content.Headers.ContentType.MediaType = "applicationType/json";
+
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("List");
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+        }
+
+        // GET: Developer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Developer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/SkillDataController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TaskTrackingApp.Models;
+
+namespace TaskTrackingApp.Controllers
+{
+    public class SkillDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/SkillData/listskills
+        [HttpGet]
+        public IQueryable<Skill> ListSkills()
+        {
+            return db.Skills;
+        }
+
+        // GET: api/SkillData/FindSkill/5
+        [ResponseType(typeof(Skill))]
+        [HttpGet]
+        public IHttpActionResult FindSkill(int id)
+        {
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(skill);
+        }
+
+        // PUT: api/SkillData/UpdateSkill/5
+        [ResponseType(typeof(void))]
+        [HttpPost]
+        public IHttpActionResult UpdateSkill(int id, Skill skill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != skill.SkillID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(skill).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SkillExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/SkillData/AddSkill
+        [ResponseType(typeof(Skill))]
+        [HttpPost]
+        public IHttpActionResult AddSkill(Skill skill)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Skills.Add(skill);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = skill.SkillID }, skill);
+        }
+
+        // DELETE: api/SkillData/DeleteSkill/5
+        [ResponseType(typeof(Skill))]
+        [HttpPost]
+        public IHttpActionResult DeleteSkill(int id)
+        {
+            Skill skill = db.Skills.Find(id);
+            if (skill == null)
+            {
+                return NotFound();
+            }
+
+            db.Skills.Remove(skill);
+            db.SaveChanges();
+
+            return Ok(skill);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool SkillExists(int id)
+        {
+            return db.Skills.Count(e => e.SkillID == id) > 0;
+        }
+    }
+}
