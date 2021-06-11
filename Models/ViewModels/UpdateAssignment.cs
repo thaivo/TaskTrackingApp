dad797d99f8a2f42cc5b3e9a1b62f46d7a987f76
thaivo <thai2903@gmail.com>
2021-06-10 22:32:23 -0400

    Add UpdateAssignment ViewModel

---
+++ Models/ViewModels/UpdateAssignment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaskTrackingApp.Models.ViewModels
+{
+    public class UpdateAssignment
+    {
+        public AssignmentDto relatedAssignment { get; set; }
+        public IEnumerable<DeveloperDto> developerDtos { get; set; }
+    }
+}
